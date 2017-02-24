bd7435e66cc1bf9243c0bbc88a18d91fbddfe895
Jan van Brügge <supermanitu@gmail.com>
2017-02-24 15:27:01 +0100

    fixed all bugs, still cluncky

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,57 @@
 export type Orientation = 'Horizontal' | 'Vertical';
-export type length = number | undefined;
 
 export function createAxisGenerator(h : any) : any
 {
     const axisFn = createAxis.bind(undefined, h);
     return Object.assign(axisFn, {
-        axisTop: scale => axisFn(scale, 'Horizontal', undefined, 0),
-        axisRight: scale => axisFn(scale, 'Vertical', undefined, 0),
-        axisBottom: scale => axisFn(scale, 'Horizontal', 0, undefined),
-        axisLeft: scale => axisFn(scale, 'Vertical', 0, undefined)
+        axisTop: scale => axisFn(scale, 'Horizontal', 20, 10),
+        axisRight: scale => axisFn(scale, 'Vertical', 20, 10),
+        axisBottom: scale => axisFn(scale, 'Horizontal', 20, 10),
+        axisLeft: scale => axisFn(scale, 'Vertical', 20, 10)
     });
 }
 
-function createAxis(h: any, scale : any, orientation : Orientation, topRight: length, bottomLeft : length)
+function createAxis(h: any, scale : any, orientation : Orientation, spaceBefore: number, tickLength : number)
 {
     const fn = n => {
         const ticks = scale.ticks(n)
             .map(scale)
             .map(v => {
-                return h('line.ticks', {
-                    attrs: {
-                        x1: orientation === 'Horizontal' ? v : -bottomLeft,
-                        x2: orientation === 'Horizontal' ? v : topRight,
-                        y1: orientation === 'Vertical' ? v : -topRight,
-                        y2: orientation === 'Vertical' ? v : bottomLeft
-                    }
-                }, []);
+                return h('g', {}, [
+                    h('line', {
+                        attrs: {
+                            class: 'ticks',
+                            x1: orientation === 'Horizontal' ? v : spaceBefore,
+                            x2: orientation === 'Horizontal' ? v : spaceBefore + tickLength,
+                            y1: orientation === 'Vertical' ? v : spaceBefore,
+                            y2: orientation === 'Vertical' ? v : spaceBefore + tickLength
+                        }
+                    }, []),
+                    h('text', { attrs: {
+                        class: 'tickValue',
+                        x: orientation == 'Horizontal' ? v + 5: 2,
+                        y: orientation == 'Vertical' ? v + 5 : 2
+                    }}, [
+                        v.toFixed(0)
+                    ])
+                ]);
             });
         
         const [rangeX, rangeY] = scale.range();
 
-        return h('g.axis', {}, [
-            h('g.tickGroup', {}, [ ...ticks ]),
-            h('line.axisLine', {
+        return h('g', { attrs: {
+            class: 'axis'
+        }}, [
+            h('g', { attrs: {
+                class: 'tickGroup'
+            }}, [ ...ticks ]),
+            h('line', {
                 attrs: {
-                    x1: 0,
-                    x2: orientation === 'Horizontal' ? rangeX : 0,
-                    y1: 0,
-                    y2: orientation === 'Vertical' ? rangeY : 0
+                    class: 'axisLine',
+                    x1: orientation === 'Horizontal' ? 0 : spaceBefore + tickLength / 2,
+                    x2: orientation === 'Horizontal' ? rangeX : spaceBefore + tickLength / 2,
+                    y1: orientation === 'Vertical' ? 0 : spaceBefore + tickLength / 2,
+                    y2: orientation === 'Vertical' ? rangeY : spaceBefore + tickLength / 2
                 }
             }, [])
         ]);

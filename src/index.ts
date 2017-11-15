f08b6a0e8ea7e7fb465eccba72c0a97fab3aa816
Jan van Brügge <supermanitu@gmail.com>
2017-11-15 20:53:22 +0100

    Rewrite code to fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,130 @@
-export type Orientation = 'Horizontal' | 'Vertical';
+import h from 'snabbdom/h';
+import { VNode } from 'snabbdom/vnode';
 
-export function createAxisGenerator(h : any) : any
-{
-    const axisFn = createAxis.bind(undefined, h);
-    return Object.assign(axisFn, {
-        axisTop: scale => axisFn(scale, 'Horizontal', 20, 10),
-        axisRight: scale => axisFn(scale, 'Vertical', 20, 10),
-        axisBottom: scale => axisFn(scale, 'Horizontal', 20, 10),
-        axisLeft: scale => axisFn(scale, 'Vertical', 20, 10)
-    });
+export type Scale<T> = any;
+export type Position = 'Top' | 'Bottom' | 'Left' | 'Right';
+export interface XYPositions {
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
 }
 
-function createAxis(h: any, scale : any, orientation : Orientation, spaceBefore: number, tickLength : number)
-{
-    const fn = n => {
-        const ticks = scale.ticks(n)
-            .map(scale)
-            .map(v => {
+export interface Dimensions {
+    left: number;
+    right: number;
+    top: number;
+    bottom: number;
+}
+
+export default class AxisGenerator<T> {
+    constructor(
+        private _scale: Scale<T>,
+        private _position: Position,
+        private _ticks: [number, string | undefined] = [0, ''],
+        private _tickSize: number = 6,
+        private _padding: number = 3
+    ) {}
+
+    public scale(): Scale<T>;
+    public scale<U>(scale: Scale<U>): AxisGenerator<U>
+    public scale(scale?: any): any {
+        if(typeof scale === 'undefined') {
+            return this._scale;
+        }
+        return new AxisGenerator(scale, this._position, this._ticks, this._tickSize, this._padding);
+    }
+
+    public ticks(): [number, number];
+    public ticks(count: number, specifier?: string): AxisGenerator<T>;
+    public ticks(count?: number, specifier?: string): any {
+        if(typeof count === 'undefined') {
+            return this._ticks;
+        }
+        return new AxisGenerator(this._scale, this._position, [count, specifier], this._tickSize, this._padding);
+    }
+
+    public static axisTop<T>(scale: Scale<T>): AxisGenerator<T> {
+        return new AxisGenerator(scale, 'Top');
+    }
+
+    public static axisBottom<T>(scale: Scale<T>): AxisGenerator<T> {
+        return new AxisGenerator(scale, 'Bottom');
+    }
+
+    public static axisLeft<T>(scale: Scale<T>): AxisGenerator<T> {
+        return new AxisGenerator(scale, 'Left');
+    }
+
+    public static axisRight<T>(scale: Scale<T>): AxisGenerator<T> {
+        return new AxisGenerator(scale, 'Right');
+    }
+
+    public (dimensions: Dimensions): VNode {
+        const ticks: VNode[] = this._scale.ticks(this._ticks[0])
+            .map(value => {
                 return h('g', {}, [
-                    h('line', {
-                        attrs: {
-                            class: 'ticks',
-                            x1: orientation === 'Horizontal' ? v : spaceBefore,
-                            x2: orientation === 'Horizontal' ? v : spaceBefore + tickLength,
-                            y1: orientation === 'Vertical' ? v : spaceBefore,
-                            y2: orientation === 'Vertical' ? v : spaceBefore + tickLength
-                        }
-                    }, []),
                     h('text', { attrs: {
                         class: 'tickValue',
-                        x: orientation == 'Horizontal' ? v + 5: 2,
-                        y: orientation == 'Vertical' ? v + 5 : 2
-                    }}, [
-                        v.toFixed(0)
-                    ])
+                        x: getXForPosition(dimensions, this._position, this._padding, value),
+                        y: getYForPosition(dimensions, this._position, this._padding, value)
+                    }}, [this._scale.tickFormat(this.ticks[0], this.ticks[1])]),
+                    h('line', { attrs: {
+                        class: 'ticks',
+                        ...getXYPositions(this._position, this._tickSize, this._padding)
+                    }})
                 ]);
             });
-        
-        const [rangeX, rangeY] = scale.range();
 
-        return h('g', { attrs: {
-            class: 'axis'
-        }}, [
-            h('g', { attrs: {
-                class: 'tickGroup'
-            }}, [ ...ticks ]),
-            h('line', {
-                attrs: {
-                    class: 'axisLine',
-                    x1: orientation === 'Horizontal' ? 0 : spaceBefore + tickLength / 2,
-                    x2: orientation === 'Horizontal' ? rangeX : spaceBefore + tickLength / 2,
-                    y1: orientation === 'Vertical' ? 0 : spaceBefore + tickLength / 2,
-                    y2: orientation === 'Vertical' ? rangeY : spaceBefore + tickLength / 2
-                }
-            }, [])
-        ]);
-    };
+        return h('g', {}, [
+            getBaseLine(this._position, dimensions, this._padding, this._tickSize)]
+                .concat(ticks)
+        );
+    }
+}
 
-    return Object.assign(fn.bind(undefined, 20), { ticks: fn });
+function getBaseLine(pos: Position, dimensions: Dimensions, padding: number, tickSize: number): VNode {
+    if(pos === 'Bottom' || pos === 'Top') {
+        const y = pos === 'Bottom' ?
+            dimensions.bottom + padding + 20 :
+            dimensions.top - padding - 20;
+
+        return h('line', { attrs: {
+            class: 'axis',
+            x1: dimensions.left,
+            x2: dimensions.right,
+            y1: y,
+            y2: y
+        }});
+    }
+    const x = pos === 'Left' ?
+        dimensions.left + padding + 40 :
+        dimensions.right - padding - 40;
+
+    return h('line', { attrs: {
+        class: 'axis',
+        x1: x,
+        x2: x,
+        y1: dimensions.top,
+        y2: dimensions.bottom
+    }});
+}
+
+function getXForPosition(dimensions: Dimensions, pos: Position, padding: number, value: number): number {
+    if(pos === 'Left') { return padding; }
+    if(pos === 'Right') { return dimensions.right - padding - 40; }
+
+    return value - 20;
+}
+
+function getYForPosition(dimensions: Dimensions, pos: Position, padding: number, value: number): number {
+    if(pos === 'Bottom') { return padding; }
+    if(pos === 'Top') { return dimensions.right - padding - 40; }
+
+    return value - 20;
+
+}
+
+function getXYPositions(pos: Position, tickSize: number, padding: number): XYPositions {
+    return undefined as any;
 }
